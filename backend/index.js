cf2cc39114e55117312c7d86903f76c8118bb81d
Roméo REL <63097212+Romeorel@users.noreply.github.com>
2024-01-10 14:40:13 +0100

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -10,27 +10,24 @@ const app = express();
 // Middlware for parsing request body
 app.use(express.json());
 
-app.options('*', cors()) 
+app.use(cors(
+    {
+    origin: ['https://project-8-n9bd.vercel.app'],
+    allowedHeaders: ["Content-Type", "Authorization", "Access-Control-Allow-Methods", "Access-Control-Request-Headers"],
+    credentials: true,
+    enablePreflight: true
+}
+));
 
-// Add headers before the routes are defined
-app.use(function (req, res, next) {
+app.options('*', cors(
+    {
+    origin: ['https://project-8-n9bd.vercel.app'],
+    allowedHeaders: ["Content-Type", "Authorization", "Access-Control-Allow-Methods", "Access-Control-Request-Headers"],
+    credentials: true,
+    enablePreflight: true
+}
+)) 
 
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'https://project-8-n9bd.vercel.app');
-
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
-
-    // Pass to next layer of middleware
-    next();
-});
 
 app.get("/", (request, response) => {
     console.log(request)
